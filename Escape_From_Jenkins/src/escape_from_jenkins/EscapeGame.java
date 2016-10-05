46ce7a90fa44763b831b6fe75a07fb97b8033ecf
Brianne Koering <brianne.koering@wsu.edu>
2016-10-05 11:01:16 -0700

    created main package
    trying to ref slick.jar

---
+++ Escape_From_Jenkins/src/escape_from_jenkins/EscapeGame.java
@@ -0,0 +1,12 @@
+package escape_from_jenkins;
+import java.util.ArrayList;
+//import slick.jar;
+
+public class EscapeGame {
+
+	public static void main() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
